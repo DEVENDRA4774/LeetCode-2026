e9a61d0a13a3567e306f4f6f95b8538508b4cec9
DEVENDRA <dhandedevendra4774@gmail.com>
2026-01-29 20:45:00 +0530

    Time: 2 ms (99.48%) | Memory: 46.3 MB (83.26%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,26 +1,32 @@
 class Solution {
     public int romanToInt(String s) {
-        // 1. Initialize the map (using a Map since 'mp' isn't a primitive in Java)
-        Map<Character, Integer> mp = new HashMap<>();
-        mp.put('I', 1);
-        mp.put('V', 5);
-        mp.put('X', 10);
-        mp.put('L', 50);
-        mp.put('C', 100);
-        mp.put('D', 500);
-        mp.put('M', 1000);
-
         int ans = 0;
         int n = s.length();
 
         for (int i = 0; i < n; i++) {
-            // 2. Use .charAt() and add a boundary check for i + 1
-            if (i < n - 1 && mp.get(s.charAt(i)) < mp.get(s.charAt(i + 1))) {
-                ans -= mp.get(s.charAt(i));
+            int current = getValue(s.charAt(i));
+            
+            // If the next value is larger, subtract the current instead of adding
+            if (i < n - 1 && current < getValue(s.charAt(i + 1))) {
+                ans -= current;
             } else {
-                ans += mp.get(s.charAt(i));
+                ans += current;
             }
         }
         return ans;
+    }
+
+    // A simple helper method is faster and cleaner than a Map
+    private int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
