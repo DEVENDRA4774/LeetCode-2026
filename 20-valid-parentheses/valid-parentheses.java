b3256e33dd0bd415781af1f8fe4a8f9a58a05868
DEVENDRA <dhandedevendra4774@gmail.com>
2026-02-01 21:35:45 +0530

    Time: 3 ms (87.91%) | Memory: 43.1 MB (82.50%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>(); // 👈 THIS LINE WAS MISSING
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+}
