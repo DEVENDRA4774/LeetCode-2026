dfee6193c5d5b37146a9f76d4cb9ac53b6e2a1a0
DEVENDRA <dhandedevendra4774@gmail.com>
2026-02-15 19:15:40 +0530

    Time: 1 ms (96.73%) | Memory: 42.9 MB (21.79%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        long left = 1, right = x;
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            
+            if (mid * mid > x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return (int) right;
+    }
+}
